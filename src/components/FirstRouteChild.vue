510c974dae0e950d12b864bab98f03d8c4c3ec18
leenzze <withoutyou08@naver.com>
2017-11-10 14:16:35 +0900

    v-for practice

--- src/components/FirstRouteChild.vue
+++ src/components/FirstRouteChild.vue
@@ -1,15 +1,25 @@
 <template>
   <div>
-    <h1>{{ msg }}</h1>
+    <!-- key값 을 사용하는 이유 앵귤러의 trackby 와 유사. 각각의 고유한 값을 주기위함 -->
+    <h1 v-for="(value, key, index) in list" :key="index">
+      {{index}} : {{key}} = {{value}}
+      </h1>
   </div>
 </template>
 
 <script>
+var list = {
+  name: 'leenzze',
+  weight: 'secret',
+  height: 'secret',
+  eyeColor: 'brown',
+  favoriteFood: 'all'
+}
 export default {
   name: 'firstRouteChild',
   data () {
     return {
-      msg: 'firstRouteChild'
+      list: list
     }
   }
 }
