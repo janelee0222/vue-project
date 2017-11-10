735f04ef492e739dca231923aebc3e84ea17846e
leenzze <withoutyou08@naver.com>
2017-11-10 18:05:20 +0900

    클래스 적용 연습

--- src/components/FirstRoute.vue
+++ src/components/FirstRoute.vue
@@ -1,8 +1,18 @@
 <template>
-  <div>
+  <div>  
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
     <h1>{{ msg }}</h1>
     Hello {{$route.params.name}}
     <router-view/>
+    <div class="container text-center">
+        <div class="box" :class="{'red': color, 'blue':!color}"></div>
+        <div class="box" :class="[color ? 'purple': 'green']"></div>
+        <div class="box" :class="{'red': color, 'blue':!color}"></div>
+        <div class="box" :class="{'purple': color, 'green':!color}"></div>
+        <button v-on:click="flipColor" class="btn btn-block btn-success">
+            Flip Color!
+        </button>
+    </div>
   </div>
 </template>
 
@@ -11,8 +21,41 @@ export default {
   name: 'firstRoute',
   data () {
     return {
-      msg: 'first route'
+      msg: 'first route',
+      color: true
+    }
+  },
+  methods: {
+    flipColor: function () {
+      this.color = !this.color
     }
   }
 }
 </script>
+
+<style>
+    .red {
+        background: red;
+    }
+
+    .blue {
+        background: blue;
+    }
+
+    .purple {
+        background: purple;
+    }
+
+    .green {
+        background: green;
+    }
+
+    .box {
+        float: left;
+        width: 200px;
+        height: 200px;
+        margin: 40px;
+        border: 1px solid rgba(0, 0, 0, .2);
+    }
+</style>
+
