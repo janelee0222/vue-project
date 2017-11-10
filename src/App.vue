ef88f18f2a93c858f8679c7f03f425f100648928
leenzze <withoutyou08@naver.com>
2017-11-10 09:41:41 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <div>
+      <ul>
+        <li><router-link to="/">Home</router-link></li>
+        <li><router-link to="/firstroute/Peter">firstroute</router-link></li>
+        <li><router-link to="/firstroute/Peter/child">firstrouteChild</router-link></li>
+      </ul>
+    </div>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/components/FirstRoute.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>{{ msg }}</h1>
+    Hello {{$route.params.name}}
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'firstRoute',
+  data () {
+    return {
+      msg: 'first route'
+    }
+  }
+}
+</script>
